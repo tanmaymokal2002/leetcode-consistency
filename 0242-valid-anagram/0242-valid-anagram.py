3e6aec55b74e34dd117de63fe16562d601303af0
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-04 19:27:26 +0530

    Time: 17 ms (26.18%), Space: 16.7 MB (85.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,7 @@
 class Solution:
+
+    #brute force approach = using sorting:
+    '''
     def isAnagram(self, s: str, t: str) -> bool:
         str1 = sorted(s)
         str2 = sorted(t)
@@ -6,4 +9,42 @@ class Solution:
             return True
         else:
             return False
+    '''
+
+    #Approach 1 = using map:
+    '''
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hashMap = {}
+        for char in s:
+            hashMap[char] = hashMap.get(char, 0) + 1
+        for char in t:
+            if char not in hashMap or hashMap[char] == 0:
+                return False
+        return True
+    '''
+
+    #Approach 2 = using ASCII values:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = [0] * 26
+        if len(s)!=len(t):
+            return False
+        for char in s:
+            count[ord(char) - ord('a')] += 1
+        for char in t:
+            if count[ord(char) - ord('a')] == 0:
+                return False
+            count[ord(char) - ord('a')] -= 1
+        return True
+
+        
+
+    
+        
+        
+
+
+
+
         
