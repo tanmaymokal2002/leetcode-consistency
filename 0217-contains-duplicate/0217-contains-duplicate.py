e508147c9736ef541e0d5c3d81bc052c3f5811d6
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-04 11:51:49 +0530

    Time: 19 ms (18.85%), Space: 34 MB (19.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,48 @@
+
 class Solution:
+
+    #brute force approach: using two loops
+    '''
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i]==nums[j]:
+                    return True
         return False
+    '''
+
+    #better approach: using sorting
+    '''
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        n=len(nums)
+        for i in range(n-1):
+            if nums[i]==nums[i+1]:
+                return True
+        return False
+    '''
+
+    #optimal approach: using hashset
+    '''
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashSet = set()
+        for num in nums:
+            if num in hashSet:
+                return True
+            hashSet.add(num)
+        return False
+    '''
+
+    #optimal approach: using hashMap
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashMap = {}
+        for num in nums:
+            if num in hashMap and hashMap[num] >= 1:
+                return True
+            hashMap[num] = hashMap.get(num, 0) + 1
+        return False
+
+
+
+
