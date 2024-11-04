e63831ee6a1bf89538620c5ccf77aa91e1df1af9
Tanmay Mokal <108115856+tanmaymokal2002@users.noreply.github.com>
2024-11-04 12:09:42 +0530

    Time: 18 ms (22.89%), Space: 17.5 MB (6.21%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        str1 = sorted(s)
+        str2 = sorted(t)
+        if str1==str2:
+            return True
+        else:
+            return False
+        
